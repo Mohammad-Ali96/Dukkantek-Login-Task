7933459dbf17fd495a8219f6886d162ff172ded3
Mohammad-Ali96 <ali96mohammad96@gmail.com>
2022-04-13 11:09:02 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:dukkantek_task/injection.dart';
 import 'package:dukkantek_task/src/app.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
-
+// Author Mohammad Ali
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
